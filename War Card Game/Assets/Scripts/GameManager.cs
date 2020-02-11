b7d6e53ed3e543cc1bbeee572c08509824fbb74b
oscarc04 <55443597+oscarc04@users.noreply.github.com>
2020-02-11 15:23:23 -0500

    changed numbering and color

---
+++ War Card Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+public Image playerCard;
+public Image cpuCard;
+
+public TextMeshProUGUI playerText;
+public TextMeshProUGUI cpuText;
+
+public int playerScore;
+public int cpuScore;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnDealButtonPressed(){
+    	int left = Random.Range(2, 15);
+    	int right = Random.Range(2, 15);
+
+    	playerCard.sprite = Instantiate(Resources.Load<Sprite>("card" + left));
+    	cpuCard.sprite = Instantiate(Resources.Load<Sprite>("card" + right));
+	
+    	if(left > right){
+    	   playerScore = playerScore + 1;
+	   playerText.text = "Player: " + playerScore;
+	   
+	}
+	else if (right > left){
+                cpuScore = cpuScore + 1;
+        cpuText.text = "Cpu: " + cpuScore;
+           
+	}
+
+    }
+}
